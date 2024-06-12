4b02b6ffc8d51a52a83ff70db3b331464376d09e
nifbastiani <108945028+nifbastiani@users.noreply.github.com>
2024-06-12 08:11:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector("#btnLogin");
+const inputUsuario = document.querySelector("#usuario");
+const inputSenha = document.querySelector("#senha");
+
+(()=>{
+   let usuario = localStorage.getItem("usuario");
+   if(usuario){
+     window.location.href = "http://www.uol.com.br";
+   }
+})();
+
+btn.onclick = (e) =>{
+    e.preventDefault();
+    let usuario = inputUsuario.value;
+    let senha = inputSenha.value;
+    if(!usuario && !senha){
+        inputUsuario.focus();
+        return;
+    }
+if(usuario === "meg"){
+    if(senha === "123"){
+        localStorage.setItem("usuario",usuario);
+        window.location.href = "http://www.uol.com.br";
+    }else{
+        inputUsuario.focus();
+    }
+}
+
+     
+}
